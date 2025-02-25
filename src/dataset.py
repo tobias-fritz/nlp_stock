3678acd02625370dffe16a98b0006713c719a2fa
Tobias Fritz <fritz.tobias@mail.de>
2025-02-25 16:30:41 +0100

    Add visualization method and establish dataset creation in SentimentDataset class

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,6 @@
 import torch
 import pandas as pd
+import numpy as np
 from torch.utils.data import Dataset, DataLoader
 
 
@@ -28,7 +29,7 @@ class SentimentDataset(Dataset):
     ]
     '''
 
-    def __init__(self, data):
+    def __init__(self, data: list):
         self.data = data
 
     def __len__(self):
@@ -46,19 +47,85 @@ class SentimentDataset(Dataset):
     def load(file_path):
         return torch.load(file_path)
 
-def create_sentiment_dataset(result_df: pd.DataFrame) -> SentimentDataset:
-    '''Create a SentimentDataset from the result DataFrame.'''
+    def visualize_datset(self):
+        '''Visualize the correlation between the inputs and the target.'''
+        import matplotlib.pyplot as plt
+        inputs = np.array([data['inputs'] for data in self.data])
+        target = np.array([data['target'] for data in self.data])
+        fig, axs = plt.subplots(2, 1, figsize=(10, 10))
+        axs[0].plot(inputs[:, 0, 0], target, 'o', label='Company')
+        axs[0].plot(inputs[:, 0, 1], target, 'o', label='Product')
+        axs[0].set_title('Positive Ratios vs Close Price')
+        axs[0].set_xlabel('Positive Ratios')
+        axs[0].set_ylabel('Close Price')
+        axs[0].legend()
+        axs[1].plot(inputs[:, 1, 0], target, 'o', label='Company')
+        axs[1].plot(inputs[:, 1, 1], target, 'o', label='Product')
 
-    raise NotImplementedError("Implement this function")
+        axs[1].set_title('Positive Ratios vs Close Price')
+        axs[1].set_xlabel('Positive Ratios')
+        axs[1].set_ylabel('Close Price')
+        axs[1].legend()
 
+        plt.show()
+
+def create_sentiment_dataset(result_df: pd.DataFrame) -> list:
+    '''Create a SentimentDataset from the result DataFrame.
+
+    Return a dataset of the news articles and their sentiments of the last 
+    month built from the result DataFrame. We will roll over the data so that
+    the inputs are sentiments of day 1, 2, and 3 and the target is the close of day 4.
+    The next data point will be the sentiments of day 2, 3, and 4 and the target 
+    is the close of day 5, and so on.
+
+    The shape of the frame should be like here and compatible with the SentimentDataset:
+    
     data = [
         {
             'inputs': [
-                [row['positive_ratio_1'], row['positive_ratio_2']],
-                [row['article_count_1'], row['article_count_2']],
+                [positive_ratios_company[0], positive_ratios_product[0]],
+                [positive_ratios_company[1], positive_ratios_product[1]],
+                [positive_ratios_company[2], positive_ratios_product[2]]
             ],
-            'target': row['Close']
+            'target': close_day_4
+        },
+        {
+            'inputs': [
+                [positive_ratios_company[0], positive_ratios_product[0]],
+                [positive_ratios_company[1], positive_ratios_product[1]],
+                [positive_ratios_company[2], positive_ratios_product[2]]
+            ],
+            'target': close_day_4
         }
-        for _, row in result_df.iterrows()
+        ...
     ]
-    return SentimentDataset(data)
+
+    Args:
+    result_df (pd.DataFrame): The result DataFrame containing the processed sentiment data.
+
+    Returns:
+    list: A list of dictionaries containing the inputs and target for each data point
+    
+    '''
+
+
+    if result_df.empty:
+        raise ValueError("The result DataFrame is empty.")
+    
+    all_results_list = []
+    for i in range(2, len(result_df)-1):
+        result_slice = result_df[i-2:i+1]
+        close_day_4 = result_df.iloc[i+1]['Close']
+        positive_ratios_1 = result_slice['positive_ratio_1'].values
+        positive_ratios_2 = result_slice['positive_ratio_2'].values
+        
+        day1 = [positive_ratios_1[0], positive_ratios_2[0]]
+        day2 = [positive_ratios_1[1], positive_ratios_2[1]]
+        day3 = [positive_ratios_1[2], positive_ratios_2[2]]
+
+        all_results_list.append({
+            'inputs': [day1, day2, day3],
+            'target': close_day_4
+        })
+
+    return all_results_list
